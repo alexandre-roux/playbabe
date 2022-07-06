3a27ed3534c48bbec29a85572154d16ace95cbac
Alexandre  Roux <aroux@softbankrobotics.com>
2022-07-06 16:12:56 +0200

    Add Eshop container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import About from "./containers/About";
+import Eshop from "./containers/Eshop";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/eshop" element={<Eshop />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </Router>
---
+++ src/containers/Eshop/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+import "./index.scss";
+
+class Eshop extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <h1>Bonjour :)</h1>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Eshop;
