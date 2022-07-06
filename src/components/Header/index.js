4a612ad3082a6897930e2dec80fce6f6f6a1d54b
Alexandre  Roux <aroux@softbankrobotics.com>
2022-07-06 13:11:03 +0200

    Add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="logo-black"
+        src="https://res.cloudinary.com/dyj1ddjba/image/upload/v1657105425/playbabe/logo-black_fwmu6d.png"
+        alt="logo-white"
+      />
+    </div>
+  );
+};
+
+export default Header;
--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
 
 import "./index.scss";
+import Header from "../../components/Header";
 
 class About extends Component {
   render() {
-    return <h1>Bonjour :)</h1>;
+    return (
+      <>
+        <Header />
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+        <h1>Bonjour :)</h1>
+      </>
+    );
   }
 }
 
