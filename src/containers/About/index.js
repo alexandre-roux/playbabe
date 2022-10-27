92f2d45a4dab0e2414f1450f2c12b6675629b6b9
Alexandre  Roux <aroux@softbankrobotics.com>
2022-10-27 11:40:33 +0200

    About page mobile second version

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -12,35 +12,79 @@ const About = () => {
   return (
     <>
       <Header />
-      {isMobile ? (
-        <div className="about-mobile"></div>
-      ) : (
-        <div className="about">
-          <div className="images">
-            <img
-              src="https://res.cloudinary.com/dyj1ddjba/image/upload/v1657116945/playbabe/calendar_il5z7e.png"
-              alt="calendar-preview"
-            />
+      <div className="about">
+        {isMobile ? (
+          <div className="about-mobile">
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
+            <h1>Bonjour :)</h1>
           </div>
-          <div className="text">
-            <div className="text-container">
-              <p>
-                This project questions the ways in which female bodies are
-                perceived in society. Articulated as political territories up to
-                this day, the PlayBabe Calendar unveils bodies through sensual
-                images, away from the male gaze.
-              </p>
-              <p>
-                Inspired by the aesthetic of erotic magazines, it aims to
-                empower women, to regain their identities.
-              </p>
-              <div className="button-container">
-                <Link to="/eshop">ORDER ONE EXEMPLAR</Link>
+        ) : (
+          <div className="about-desktop">
+            <div className="images">
+              <img
+                src="https://res.cloudinary.com/dyj1ddjba/image/upload/v1657116945/playbabe/calendar_il5z7e.png"
+                alt="calendar-preview"
+              />
+            </div>
+            <div className="text">
+              <div className="text-container">
+                <p>
+                  This project questions the ways in which female bodies are
+                  perceived in society. Articulated as political territories up
+                  to this day, the PlayBabe Calendar unveils bodies through
+                  sensual images, away from the male gaze.
+                </p>
+                <p>
+                  Inspired by the aesthetic of erotic magazines, it aims to
+                  empower women, to regain their identities.
+                </p>
+                <div className="button-container">
+                  <Link to="/eshop">ORDER ONE EXEMPLAR</Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
       <Footer />
     </>
   );
