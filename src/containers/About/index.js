995250891851f43c31445a048dcd2d45076a0100
Alexandre  Roux <aroux@softbankrobotics.com>
2022-11-29 20:33:04 +0100

    Update About page before parallax

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -5,6 +5,7 @@ import Footer from "../../components/Footer";
 import "./index.scss";
 import { useMediaQuery } from "react-responsive";
 import Button from "../../components/Button";
+import { Link } from "react-router-dom";
 
 const About = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 1000px)" });
@@ -67,10 +68,19 @@ const About = () => {
         </div>
       ) : (
         <div className="about-desktop">
-          <Header />
+          <Header classname="header" />
           <div className="about-content">
             <div className="images">
-              <img src="/images/calendar.png" alt="calendar-preview" />
+              <img
+                className="about1"
+                src="/images/about/about1.jpg"
+                alt="about1"
+              />
+              <img
+                className="about3"
+                src="/images/about/about3.jpg"
+                alt="about3"
+              />
             </div>
             <div className="text">
               <div className="text-container">
@@ -83,45 +93,34 @@ const About = () => {
                   reclaim the traditional use of women's bodies in our culture
                   and place it a the center of our political project.
                 </p>
+                <p style={{ fontWeight: "bold" }}>
+                  This year PLAYBABE supports Sex Workers' Rights and give 100%
+                  of Calendar sale benefits to{" "}
+                  <span style={{ textDecorationLine: "underline" }}>
+                    HYDRA BERLIN.
+                  </span>
+                </p>
                 <div className="details">
+                  <Link to={"/eshop"}>
+                    <img src="/images/about/button.PNG" alt="button" />
+                  </Link>
                   <p>
-                    Formats we offer
-                    <br />
-                    / A3 Calendar, limited edition, number and reprint possible
-                    <br />
-                    / Unique upcycled underwear designed by Lucy
-                    <br />
-                    / Upcycled Tee shirts
-                    <br />
-                    / Risography prints
-                    <br />/ Postcards
-                  </p>
-                  <p>
-                    100% of our sales benefits will be given this year to HYDRA
-                    BERLIN for our 2023 edition.
-                    <br />
-                    Read more about the association here:
-                    https://www.hydra-berlin.de/
-                  </p>
-                  <p>
-                    Reach out to us:
-                    <br />
-                    info@playbabe.com
-                    <br />
                     Aurélia Majean
                     <br />
-                    +33 670875219
+                    & Lucy Owen Jones
                     <br />
-                    Lucy Owen Jones
+                    Founders of PLAYBABE
                     <br />
-                    +49 159 01071248
+                    <br />
+                    <span style={{ fontWeight: "bold" }}>
+                      info@playbabe.org
+                    </span>
                   </p>
                 </div>
-                <Button text="ORDER ONE EXEMPLAR" to="/eshop" />
               </div>
             </div>
           </div>
-          <Footer />
+          <Footer classname="footer" />
         </div>
       )}
     </>
