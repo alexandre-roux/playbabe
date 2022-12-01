8276e031678c82a4faaaff0912b7daa8d65a1173
Alexandre  Roux <aroux@softbankrobotics.com>
2022-12-01 20:08:43 +0100

    Second version of ProductModal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import About from "./containers/About";
 import Eshop from "./containers/Eshop";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faXmark);
 
 function App() {
   return (
--- src/components/ProductModal/index.js
+++ src/components/ProductModal/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ProductModal = ({ name, price, setDisplayModal }) => {
   return (
     <>
       <div className="modal-overlay" onClick={() => setDisplayModal(false)}>
-        <div className="modal-container"></div>
+        <div className="modal-container">
+          <FontAwesomeIcon
+            icon="xmark"
+            className="xmark"
+            onClick={() => setDisplayModal(false)}
+          />
+          <div className="modal-content">
+            <div className="images">
+              <img src="/images/eshop/calendar1.jpg" alt="calendar" />
+            </div>
+            <div className="text"></div>
+          </div>
+        </div>
       </div>
     </>
   );
