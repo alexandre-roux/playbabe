8c067d660cd94571ff1d49ce0edd3d39dd7cb2cd
Alexandre Roux <alexandre.roux@ensc.fr>
2024-05-27 17:19:09 +0200

    Refactor image rendering logic in Home component

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,218 +1,83 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 import "./index.scss";
 
 const Home = () => {
-  const [background, setBackground] = useState("homepage1");
+    const images = [
+        "homepage1", "homepage2", "homepage3", "homepage4", "homepage5",
+        "homepage6", "homepage7", "homepage8", "homepage9", "homepage10",
+        "homepage11", "homepage12", "homepage13", "homepage14", "homepage15",
+        "homepage16", "homepage17", "homepage18", "homepage19"
+    ];
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setBackground("homepage" + Math.floor(Math.random() * 19 + 1));
-    }, 1000);
+    const [background, setBackground] = useState("homepage1");
 
-    return () => clearInterval(intervalId);
-  }, []);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setBackground("homepage" + Math.floor(Math.random() * 19 + 1));
+        }, 1000);
 
-  const [isHovering, setIsHovering] = useState(false);
-  const colors = ["#FF1739", "#DEFF42"];
-  const [textColor, setTextColor] = useState(colors[0]);
+        return () => clearInterval(intervalId);
+    }, []);
 
-  const handleMouseOver = () => {
-    setIsHovering(true);
-    setTextColor(colors[Math.floor(Math.random() * colors.length)]);
-  };
+    const [isHovering, setIsHovering] = useState(false);
+    const colors = ["#FF1739", "#DEFF42"];
+    const [textColor, setTextColor] = useState(colors[0]);
 
-  const handleMouseOut = () => {
-    setIsHovering(false);
-  };
+    const handleMouseOver = () => {
+        setIsHovering(true);
+        setTextColor(colors[Math.floor(Math.random() * colors.length)]);
+    };
 
-  return (
-    <div className="home-container">
-      <img
-        src="/images/homepage/homepage1.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage1" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage2.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage2" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage3.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage3" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage4.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage4" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage5.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage5" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage6.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage6" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage7.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage7" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage8.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage8" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage9.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage9" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage10.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage10" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage11.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage11" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage12.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage12" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage13.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage13" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage14.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage14" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage15.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage15" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage16.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage16" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage17.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage17" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage18.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage18" ? "block" : "none",
-        }}
-      />
-      <img
-        src="/images/homepage/homepage19.jpg"
-        alt="background"
-        style={{
-          display:
-            background === "" || background === "homepage19" ? "block" : "none",
-        }}
-      />
-      <Link to="/about" className="text-container">
-        <h2
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-          style={{
-            color: isHovering ? textColor : "white",
-          }}
-        >
-          ENTER WEBSITE
-        </h2>
-        <h1
-          className="logo"
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-          style={{
-            color: isHovering ? textColor : "white",
-          }}
-        >
-          PLAYBABE
-        </h1>
-        <h2
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-          style={{
-            color: isHovering ? textColor : "white",
-          }}
-        >
-          ENTER WEBSITE
-        </h2>
-      </Link>
-    </div>
-  );
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    };
+
+    return (
+        <div className="home-container">
+            {images.map((img, index) => (
+                <img
+                    key={index}
+                    src={`/images/homepage/${img}.jpg`}
+                    alt="background"
+                    style={{
+                        display: background === "" || background === img ? "block" : "none",
+                    }}
+                />
+            ))}
+            <Link to="/about" className="text-container">
+                <h2
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                    style={{
+                        color: isHovering ? textColor : "white",
+                    }}
+                >
+                    ENTER WEBSITE
+                </h2>
+                <h1
+                    className="logo"
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                    style={{
+                        color: isHovering ? textColor : "white",
+                    }}
+                >
+                    PLAYBABE
+                </h1>
+                <h2
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                    style={{
+                        color: isHovering ? textColor : "white",
+                    }}
+                >
+                    ENTER WEBSITE
+                </h2>
+            </Link>
+        </div>
+    );
 };
 
 export default Home;
